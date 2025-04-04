3604eb6cb5a8ce08f9c52fef301526fe4b792721
Gabriel A. Moreno M. <166923741+EldritchWeaver@users.noreply.github.com>
2025-04-04 13:35:24 -0400

    cerregido

--- practica_3.ps1
+++ practica_3.ps1
@@ -2,12 +2,15 @@ $cuerpo = @{
              "phone"=$telefono
              "message"=$mensaje
              "key"="textbelt"
-$envio.content
-$envio = invoke-webrequest -uri https://textbelt.com/text -body $cuerpo -method post
+}
+
 $telefono = read-host "indicame el numero de telefono sin espacios ni guiones iniciando con el signo + y el codigo del pais:"
 write-output ""
+
 $mensaje = read-host "indicame el mensaje del destinatario :"
 write-output ""
 
+$envio = invoke-webrequest -uri https://textbelt.com/text -body $cuerpo -method post
+$envio.content
+
 read-host "presione cualquier tecla para salir"
-}
